c87e0343397d291e6580a6761ab32e3f04162887
Alex de Abreu <alex.abreu.softwaredeveloper@gmail.com>
2024-05-21 11:57:26 -0300

    att terça

--- Aulas DWEB/api_rest/src/controllers/instructors.js
+++ Aulas DWEB/api_rest/src/controllers/instructors.js
@@ -1,51 +1,102 @@
-const { instructors } = require("../database");
+let { instructors } = require("../database");
 
-function listInstructors (req, res) {
-    return res.status(200).json(instructors);
+function listInstructors(req, res) {
+  return res.status(200).json(instructors);
+}
+
+function listInstructor(req, res) {
+  const { id } = req.params;
+  const result = instructors.find((instructor) => {
+    return instructor.id === Number(id);
+  })
+
+  if (!result) {
+    return res.status(404).json({ message: 'Instructor not found' });
+  }
+  return res.status(200).json(result);
+}
+
+function registerInstructor(req, res) {
+  //console.log(req.body);
+  const { name, email, status } = req.body
+
+  if (!name) {
+    return res.status(400).json({ message: "The Name field is required." })
   }
 
-  function listInstructor (req, res) {
-    const {id} = req.params;
-
-    const result = instructors.find((instructor) => {
-        return instructor.id === Number(id);
-    })
-
-    if(!result) {
-        return res.status(404).json({message: 'Instructor not found'});
-    }
-    return res.status(200).json(result);
+  if (!email) {
+    return res.status(400).json({ message: "The Email field is required." })
   }
 
-  function registerInstructor (req,res) {
-    //console.log(req.body);
-    const { nome, email, status } = req.body
-
-    if(!nome) {
-      return res.status(400).json({message: "The Name field is required." })
-    }
-
-    if(!email) {
-      return res.status(400).json({message: "The Email field is required." })
-    }
-
-    if(!status) {
-      return res.status(400).json({message: "The Status field is required." })
-    }
-
-    const instructor ={
-      nome: nome,
-      email: email,
-      status: status
-    }
-
-    instructors.push(instructor);
-
-    return res.status(201).json(instructor);
+  if (!status) {
+    return res.status(400).json({ message: "The Status field is required." })
   }
 
-  module.exports = {
-     listInstructors,
-     listInstructor,
-     registerInstructor
+  const instructor = {
+    name: name,
+    email: email,
+    status: status
   }
+
+  instructors.push(instructor);
+
+  return res.status(201).json(instructor);
+}
+
+function updateInstructor(req, res) {
+  const { id } = req.params;
+  const { name, email, status } = req.body
+
+  const result = instructors.find((instructor) => {
+    return instructor.id === Number(id);
+  })
+
+  if (!result) {
+    return res.status(404).json({ message: 'Instructor not found' });
+  }
+
+  if (!name) {
+    return res.status(400).json({ message: "The Name field is required." })
+  }
+
+  if (!email) {
+    return res.status(400).json({ message: "The Email field is required." })
+  }
+
+  if (!status) {
+    return res.status(400).json({ message: "The Status field is required." })
+  }
+
+  result.name = name;
+  result.email = email;
+  result.status = status;
+
+  return res.status(204).send (result);
+}
+
+function deleteInstructor (req, res) {
+  const { id } = req.params;
+
+  const result = instructors.find((instructor) => {
+    return instructor.id === Number(id);
+  })
+
+  if (!result) {
+    return res.status(404).json({ message: 'Instructor not found' });
+  }
+
+  instructors = instructors.filter((instructor) => {
+    return instructor.id !== Number(id);
+  })
+
+  return res.status(200).json({message: 'successfully deleted instructor'})
+}
+
+
+module.exports = {
+  listInstructors,
+  listInstructor,
+  registerInstructor,
+  updateInstructor,
+  deleteInstructor
+}
--- Aulas DWEB/api_rest/src/routes.js
+++ Aulas DWEB/api_rest/src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { listInstructors, listInstructor, registerInstructor } = require('./controllers/instructors');
+const { listInstructors, listInstructor, registerInstructor, updateInstructor, deleteInstructor } = require('./controllers/instructors');
 
 const routes = express();
 
 routes.get('/', listInstructors);
 routes.get('/instructor/:id', listInstructor);
 routes.post('/instructors', registerInstructor);
+routes.put('/instructors/:id', updateInstructor);
+routes.delete('/instructors/:id', deleteInstructor);
 
 module.exports = routes
