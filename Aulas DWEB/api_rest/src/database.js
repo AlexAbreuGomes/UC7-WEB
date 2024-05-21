96fef4d3f241e567fca2b7080a7e02b5a56065c1
AlexAbreu <alex.abreu.softwaredeveloper@gmail.com>
2024-05-21 20:35:11 -0300

    att home 21/05

--- Aulas DWEB/api_rest/src/database.js
+++ Aulas DWEB/api_rest/src/database.js
@@ -11,6 +11,13 @@ const dataBase = {
             name: 'valtemir',
             email: 'vava@gmail.com',
             status: true
+        },
+
+        {
+            id: 3,
+            name: 'val',
+            email: 'vava@mail.com',
+            status: true
         }
     ]
 }
